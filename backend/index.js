ffb9b0de7da156bc632a789a037ea39ea83c8177
Noel Rojas <narojasgalviz@gmail.com>
2022-10-22 10:37:52 -0400

    adding connection to db

--- backend/index.js
+++ backend/index.js
@@ -6,15 +6,24 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const path = require("path");
+const userRouter = require("./routes/userRouter.js");
 
 const port = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/user", (req, res) => {
+// connect to database
+mongoose.connect("mongodb://localhost:27017/hackathon-2022", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get("/", (req, res) => {
   res.send("<h1>Hello World</h1>");
 });
+
+app.use("/users", userRouter);
 
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
